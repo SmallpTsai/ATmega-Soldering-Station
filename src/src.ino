6daefc9eaf501bf9c7abcdd6f5f50739fafa8cf6
Smallp Tsai <smallptsai@gmail.com>
2023-06-04 21:24:02 +0800

    adjust calibration screen

--- src/src.ino
+++ src/src.ino
@@ -147,6 +147,7 @@ bool      BodyFlip    = BODYFLIP;
 bool      ECReverse   = ECREVERSE;
 
 // Default values for tips
+const uint16_t TargetTemp[3] = {200, 280, 360};
 uint16_t  CalTemp[TIPMAX][4] = {TEMP200, TEMP280, TEMP360, TEMPCHP};
 char      TipName[TIPMAX][TIPNAMELENGTH] = {TIPNAME};
 uint8_t   CurrentTip   = 0;
@@ -175,8 +176,9 @@ const char *BoostTempItems[]   = { "Boost Temp", "\xB0""C" };
 const char *SleepTimerItems[]  = { "Sleep Timer", "Minutes" };
 const char *OffTimerItems[]    = { "Off Timer", "Minutes" };
 const char *BoostTimerItems[]  = { "Boost Timer", "Seconds" };
-const char *DeleteMessage[]    = { "Warning", "You cannot", "delete your", "last tip!" };
-const char *MaxTipMessage[]    = { "Warning", "You reached", "maximum number", "of tips!" };
+const char *DeleteMessage[]    = { "Warning:", "You cannot", "delete your", "last tip!" };
+const char *MaxTipMessage[]    = { "Warning:", "You reached", "maximum number", "of tips!" };
+const char *CancelCalibMsg[]   = { "", "Calibration", "is cancelled.", "" };
 
 // Variables for pin change interrupt
 volatile uint8_t  a0, b0, c0, d0;
@@ -187,7 +189,7 @@ volatile bool     handleMoved;
  
 // Variables for temperature control
 uint16_t  SetTemp, ShowTemp, gap, Step;
-double    Input, Output, Setpoint, RawTemp, CurrentTemp, ChipTemp;
+double    Input, Output, Setpoint, RawTemp, CurrentTemp;
 
 // Variables for voltage readings
 uint16_t  Vcc, Vin;
@@ -268,7 +270,6 @@ void setup() {
   // read and set current iron temperature
   SetTemp  = DefaultTemp;
   RawTemp  = denoiseAnalog(SENSOR_PIN);
-  ChipTemp = getChipTemp();
   calculateTemp();
 
   // turn on heater if iron temperature is well below setpoint
@@ -302,6 +303,7 @@ void loop() {
   MainScreen();       // updates the main page on the OLED
 }
 
+
 //
 //
 //
@@ -313,24 +315,13 @@ void loop() {
 //
 
 
-
 // menu screen
-uint8_t MenuScreenAdvance(const char *Title, const char *Items[], uint8_t numberOfItems, uint8_t selected) {
-  // {
-  //   Serial.println(Title);
-  //   for(uint8_t i=0;i<numberOfItems;i++)
-  //     Serial.println(Items[i]);
-  //   Serial.println(selected);
-  //   Serial.println(Items[selected]);
-  // }
+uint8_t MenuScreenAdvance(const char *Title, const char *Items[], uint8_t numberOfItems, uint8_t selected, const char *Title2) {
 
-  bool isTipScreen = strcmp(Title, "Tip:") == 0;
   uint8_t lastselected = selected;
   int8_t  arrow = 0;
   if (selected) arrow = 1;
   setRotary(0, numberOfItems - 1, 1, selected, true);
-
-  Serial.println(numberOfItems);
 
   do {
     selected = getRotary();
@@ -340,9 +331,12 @@ uint8_t MenuScreenAdvance(const char *Title, const char *Items[], uint8_t number
     do {
       u8g.setFont(u8g_font_9x15);
       u8g.setFontPosTop();
+
       // Title
       u8g.drawStr( 0, 0,  Title);
-      if (isTipScreen) u8g.drawStr( 54, 0,  TipName[CurrentTip]);
+      if (Title2)
+        u8g.drawStr( 54, 0, Title2);
+
       // Items
       u8g.drawStr( 0, 16 * (arrow + 1), ">");
       for (uint8_t i=0; i<3; i++) {
@@ -371,7 +365,7 @@ uint8_t MenuScreenAdvance(const char *Title, const char *Items[], uint8_t number
 
 // menu screen
 uint8_t MenuScreen(const char *Items[], uint8_t numberOfItems, uint8_t selected) {
-  return MenuScreenAdvance(Items[0], Items+1, numberOfItems-1, selected);
+  return MenuScreenAdvance(Items[0], Items+1, numberOfItems-1, selected, NULL);
 }
 
 
@@ -383,7 +377,7 @@ void MessageScreen(const char *Items[], uint8_t numberOfItems) {
     for (uint8_t i = 0; i < numberOfItems; i++) u8g.drawStr( 0, i * 16,  Items[i]);
   } while(u8g.nextPage());
 
-  WaitForAnyKey(NULL, 1000);
+  WaitForAnyKey(NULL, 3000);
 }
 
 
@@ -464,9 +458,10 @@ void ROTARYCheck() {
   // set working temperature according to rotary encoder value
   SetTemp = getRotary();
   
-  // check rotary encoder switch
   button.read();
-  if(button.wasLongPressed()) {
+  pushBtn1.read();
+  pushBtn2.read();
+  if(button.wasLongPressed() || pushBtn1.wasReleased()) {
     beep();
     SetupScreen();
   } else if (button.wasReleasedFromPress()) {
@@ -474,6 +469,12 @@ void ROTARYCheck() {
     inBoostMode = !inBoostMode;
     if (inBoostMode) boostmillis = millis();
     handleMoved = true;
+  } else if (pushBtn2.wasReleased()) {
+    beep();
+    inBoostMode = false;
+    handleMoved = true;
+    setRotary(TEMP_MIN, TEMP_MAX, TEMP_STEP, DefaultTemp, false);
+    SetTemp = DefaultTemp;
   }
 
   // check timer when in boost mode
@@ -697,7 +698,7 @@ void TipScreen() {
   uint8_t selection = 0;
   bool repeat = true;  
   while (repeat) {
-    selection = MenuScreen(TipItems, sizeof(TipItems)/sizeof(char*), selection);
+    selection = MenuScreenAdvance(TipItems[0], TipItems+1, sizeof(TipItems)/sizeof(char*)-1, selection, TipName[CurrentTip]);
     switch (selection) {
       case 0:   ChangeTipScreen();   break;
       case 1:   CalibrationScreen(); break;
@@ -790,10 +791,8 @@ void ChangeTipScreen() {
   const char* Items[TIPMAX] = {NULL};
   for (uint8_t i=0; i<NumberOfTips; i++) {
     Items[i] = TipName[i];
-    Serial.println(Items[i]);
   }
-  Serial.println(CurrentTip);
-  uint8_t result = MenuScreenAdvance("Select Tip", Items, NumberOfTips, CurrentTip);
+  uint8_t result = MenuScreenAdvance("Select Tip", Items, NumberOfTips, CurrentTip, NULL);
   if (result != 0xFF)
     CurrentTip = result;
 }
@@ -802,31 +801,52 @@ void ChangeTipScreen() {
 // temperature calibration screen
 void CalibrationScreen() {
   uint16_t CalTempNew[4]; 
+  bool isStable = false;
   for (uint8_t CalStep = 0; CalStep < 3; CalStep++) {
     SetTemp = CalTemp[CurrentTip][CalStep];
     setRotary(100, 500, 1, SetTemp, false);
     beepIfWorky = true;
+    isStable = false;
 
     do {
       SENSORCheck();      // reads temperature and vibration switch of the iron
+      if (isWorky) {
+        if(uint16_t(RawTemp) < TargetTemp[CalStep])
+          SetTemp += 1;
+        else if(uint16_t(RawTemp) > TargetTemp[CalStep])
+          SetTemp -= 1;
+        else
+          isStable = true;
+      }
       Thermostat();       // heater control
       
       u8g.firstPage();
       do {
         u8g.setFont(u8g_font_9x15);
         u8g.setFontPosTop();
-        u8g.drawStr( 0, 0,  F("Calibration"));
-        u8g.setPrintPos(0, 16); u8g.print(F("Step: ")); u8g.print(CalStep + 1); u8g.print(" of 3");
-        if (isWorky) {
-          u8g.setPrintPos(0, 32); u8g.print(F("Set measured"));
-          u8g.setPrintPos(0, 48); u8g.print(F("temp: ")); u8g.print(getRotary());
+        u8g.setPrintPos(0, 0); u8g.print(F("Calibrate: ")); u8g.print(CalStep + 1); u8g.print("/3");
+        u8g.setPrintPos(0, 16); u8g.print(F("Target:")); u8g.print(TargetTemp[CalStep]);
+        u8g.setPrintPos(0, 32); u8g.print(F("ADC   :")); u8g.print(uint16_t(RawTemp));
+        u8g.setPrintPos(0, 48); 
+        if (isStable) {
+          u8g.print(F("Temp  :")); u8g.print(getRotary());
         } else {
-          u8g.setPrintPos(0, 32); u8g.print(F("ADC:  ")); u8g.print(uint16_t(RawTemp));
-          u8g.setPrintPos(0, 48); u8g.print(F("Please wait..."));
+          u8g.print(F("Please wait..."));
         }
       } while(u8g.nextPage());
       button.read();
-    } while (!button.wasReleasedFromPress());
+      pushBtn1.read();
+      pushBtn2.read();
+      if (isStable && (button.wasReleasedFromPress() || pushBtn1.wasReleased()))
+        break;
+      if (pushBtn2.wasReleased()) {
+        analogWrite(CONTROL_PIN, HEATER_OFF);       // shut off heater
+        beep();
+        MessageScreen(CancelCalibMsg, sizeof(CancelCalibMsg)/sizeof(char*));
+        return;
+      }
+
+    } while (true);
 
     CalTempNew[CalStep] = getRotary();
     beep(); delay (10);
@@ -923,8 +943,10 @@ void AddTipScreen() {
     auto oldTip = CurrentTip;
     CurrentTip = NumberOfTips++;
     if(InputTipNameScreen()) {
-      CalTemp[CurrentTip][0] = TEMP200; CalTemp[CurrentTip][1] = TEMP280;
-      CalTemp[CurrentTip][2] = TEMP360; CalTemp[CurrentTip][3] = TEMPCHP;
+      CalTemp[CurrentTip][0] = TEMP200; 
+      CalTemp[CurrentTip][1] = TEMP280;
+      CalTemp[CurrentTip][2] = TEMP360; 
+      CalTemp[CurrentTip][3] = TEMPCHP;
     }
     else {
       NumberOfTips--;
